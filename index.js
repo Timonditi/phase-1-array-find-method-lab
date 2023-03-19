24635cfd7374584efd34f16a734dd96f4d9837bd
Timothy <todongoonditi@gmail.com>
2023-03-19 14:21:04 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// code your solution here
+function superbowlWin(record){
+    const win = record.find((game) => game.result === 'W');
+
+    return win ? win.year : undefined;
+}
+const broncosRecord = [
+    { year: 1998, result: 'W' },
+    { year: 1999, result: 'L' },
+    { year: 2016, result: 'W' }
+  ];
+  
+  console.log(superbowlWin(broncosRecord)); // Output: 1998
+  
